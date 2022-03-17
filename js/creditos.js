d2afa03606cd2b6e86ae0ae83280a1a549dd5f75
ayonafamily <ayonafamily@gmail.com>
2022-03-16 19:23:48 -0500

    logo whatsapp

--- js/creditos.js
+++ js/creditos.js
@@ -1,7 +1,7 @@
 const autor = () => {
     let fecha = new Date();
     const author = "<a href='mailto:ayonafamily@gmail.com' target='_blank' title='email de Jorge Ayona'><em>Jorge Ayona</em></a>";
-    const whatsapp="<a href = 'https://api.whatsapp.com/send?phone=51963837204' target='_blank'>WhatsApp</a>";    
+    const whatsapp="<a href = 'https://api.whatsapp.com/send?phone=51963837204' target='_blank'><img src ='img/whatsapp.png' width = 1.5% >Whatsapp</a>";    
     
     document.getElementById("pie").innerHTML = `&copy; ${fecha.getFullYear()}  <br> Diseño: ${author} <br> ${whatsapp} `;
 }

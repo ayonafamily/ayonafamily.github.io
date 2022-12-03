45411cdcdcfdf2060482ee90f19e273718098a0d
ayonafamily <ayonafamily@gmail.com>
2022-12-03 14:27:30 -0500

    actualizar celular

--- js/creditos.js
+++ js/creditos.js
@@ -1,9 +1,8 @@
 const autor = () => {
     let fecha = new Date();
     const author = "<a href='mailto:ayonafamily@gmail.com' target='_blank' title='email de Jorge Ayona'><em>Jorge Ayona</em></a>";
-    const whatsapp="<a href = 'https://api.whatsapp.com/send?phone=51963837204' target='_blank'><img src ='img/whatsapp.png' width = 1.8% > Whatsapp</a>";    
+    const whatsapp="<a href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><img src ='img/whatsapp.png' width = 1.8% > Whatsapp</a>";    
     
     document.getElementById("pie").innerHTML = `&copy; ${fecha.getFullYear()}  <br> Diseño: ${author} <br> ${whatsapp} `;
-}
-
+};
   autor();

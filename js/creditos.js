c3a83b7e7e59ce98f558141671a9aff6a175d51a
ayonafamily <ayonafamily@gmail.com>
2022-12-16 22:24:58 -0500

    Retoques de js

--- js/creditos.js
+++ js/creditos.js
@@ -2,12 +2,11 @@ const autor = () => {
     let fecha = new Date();
     const author = "<a><em>Jorge Ayona</em></a>";
     const qr="<img src ='img/marketin.jpeg' width= 20% >";
-    const whatsapp="<a href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><img src ='img/whatsapp.png' width = 1.8% > Whatsapp</a>";    
-    const contacto = "<a class='btn btn-primary' href='https://docs.google.com/forms/d/e/1FAIpQLSdsU-p8k6VS-8zhowHnzYotVzKLQbpc_H6Fgn8dY1s04QiMEQ/viewform?usp=sf_link' target='_blank' >Envie un mensaje</a>";
+    const whatsapp="Via :<a href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><img src ='img/whatsapp.png' width = 1.8% > Whatsapp</a>";    
+    const contacto = "Mándame un <br><a class='btn btn-primary' href='https://docs.google.com/forms/d/e/1FAIpQLSdsU-p8k6VS-8zhowHnzYotVzKLQbpc_H6Fgn8dY1s04QiMEQ/viewform?usp=sf_link' target='_blank' >mensaje</a><br>";
     
-    
-    
-    document.getElementById("pie").innerHTML = `&copy; ${fecha.getFullYear()}  <br> Diseño: ${author} <br><br>${whatsapp} <br><br> ${qr} <br><br>
-     ${contacto} `;
+       
+    document.getElementById("pie").innerHTML = `${whatsapp} <br><br> ${qr} <br><br>
+     ${contacto} <hr>&copy; ${fecha.getFullYear()} <br> Diseño: ${author}  `;
 };
   autor();

02437192a87e2f626932792fca502c0d5708c82d
ayonafamily <ayonafamily@gmail.com>
2022-12-16 22:27:09 -0500

    parrafo de presentación

--- js/creditos.js
+++ js/creditos.js
@@ -6,7 +6,7 @@ const autor = () => {
     const contacto = "Mándame un <br><a class='btn btn-primary' href='https://docs.google.com/forms/d/e/1FAIpQLSdsU-p8k6VS-8zhowHnzYotVzKLQbpc_H6Fgn8dY1s04QiMEQ/viewform?usp=sf_link' target='_blank' >mensaje</a><br>";
     
        
-    document.getElementById("pie").innerHTML = `${whatsapp} <br><br> ${qr} <br><br>
-     ${contacto} <hr>&copy; ${fecha.getFullYear()} <br> Diseño: ${author}  `;
+    document.getElementById("pie").innerHTML = `${whatsapp} <br> ${qr} <br><br>
+     ${contacto} <hr>&copy; ${fecha.getFullYear()} Diseño: ${author}  `;
 };
   autor();

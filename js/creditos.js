e051d5fc4af0961516bf8f2f7eeaaba574d8b0cc
ayonafamily <ayonafamily@gmail.com>
2023-01-24 21:03:49 -0500

    Aplicaciones de cursos

--- js/creditos.js
+++ js/creditos.js
@@ -1,8 +1,8 @@
 const autor = () => {
     let fecha = new Date();
-    const author = "<a><em>Jorge Ayona</em></a>";
+    const author = "<a href='https://jorgeayona.com'><em>Jorge Ayona</em></a>";
     const qr="<img src ='img/marketin.jpeg' width= 20% >";
-    const whatsapp="Via :<a href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><img src ='img/whatsapp.png' width = 1.8% > Whatsapp</a>";    
+    const whatsapp="Via :<a  href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><img src ='img/whatsapp.png' width = 1.8% > Whatsapp</a>";    
     const contacto = "Mándame un <br><a class='btn btn-primary' href='https://docs.google.com/forms/d/e/1FAIpQLSdsU-p8k6VS-8zhowHnzYotVzKLQbpc_H6Fgn8dY1s04QiMEQ/viewform?usp=sf_link' target='_blank' >mensaje</a><br>";
     
        

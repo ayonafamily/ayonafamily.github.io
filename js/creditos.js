2c8a2770d6bb75efe198209ff021fad59b45c22b
ayonafamily <ayonafamily@gmail.com>
2023-02-04 17:31:01 -0500

    color botón whatsapp

--- js/creditos.js
+++ js/creditos.js
@@ -1,7 +1,7 @@
 const autor = () => {
     let fecha = new Date();
     const author = "<a href='https://jorgeayona.com'><em>Jorge Ayona</em></a>";
-    const whatsapp="<a class='btn btn-outline-primary m-2' href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><i class='fa fa-whatsapp' aria-hidden='false'></i>Whatsapp</a>";    
+    const whatsapp="<a class='btn btn-outline-success m-2' href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><i class='fa fa-whatsapp' aria-hidden='false'></i>Whatsapp</a>";    
     const contacto = "<a class='btn btn-outline-primary m-2' href='https://docs.google.com/forms/d/e/1FAIpQLSdsU-p8k6VS-8zhowHnzYotVzKLQbpc_H6Fgn8dY1s04QiMEQ/viewform?usp=sf_link' target='_blank'><i class='fa fa-envelope' aria-hidden='false'></i> Escríbeme</a><br>";    
       //Escribir en documento 
     document.getElementById("pie").innerHTML = `${whatsapp} ${contacto} <br>&copy; ${fecha.getFullYear()} Diseño: ${author}  `;

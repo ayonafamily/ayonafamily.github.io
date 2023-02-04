820361a1633b056553963c3cb62011255a65d98c
ayonafamily <ayonafamily@gmail.com>
2023-02-04 07:40:06 -0500

    mejoras en ui - botones

--- js/jquery.js
+++ js/jquery.js
@@ -11,6 +11,20 @@ $("p")
         $(this).css(styles);
     });
 
+    $("ul")
+    .on("mouseenter", function () {
+        $(this).css({
+            "color": "blue"    
+        });
+    })
+    .on("mouseout", function () {
+        var styles = {            
+            color: "#000"     
+        };
+        $(this).css(styles);
+    });
+
+
 $("#alertaDescarga").on("click", function () {
     alert("Pulse 'aceptar' para descargar..");    
 });// Descarga cv

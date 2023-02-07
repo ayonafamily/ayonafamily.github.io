66dd90f8045401446bb9a0da55f45834945e6a2e
ayonafamily <ayonafamily@gmail.com>
2023-02-06 22:43:58 -0500

    cambios en imagenes

--- js/creditos.js
+++ js/creditos.js
@@ -1,6 +1,6 @@
 const autor = () => {
     let fecha = new Date();
-    const author = "<a href='https://jorgeayona.com'><em>Jorge Ayona</em></a>";
+    const author = "<a href='https://jorgeayona.com' title='Jorge Ayona'><em>Jorge Ayona</em></a>";
     const whatsapp="<a class='btn btn-outline-success m-2' href = 'https://api.whatsapp.com/send?phone=51995121512' target='_blank'><i class='fa fa-whatsapp' aria-hidden='false'></i>Whatsapp</a>";    
     const contacto = "<a class='btn btn-outline-primary m-2' href='https://docs.google.com/forms/d/e/1FAIpQLSdsU-p8k6VS-8zhowHnzYotVzKLQbpc_H6Fgn8dY1s04QiMEQ/viewform?usp=sf_link' target='_blank'><i class='fa fa-envelope' aria-hidden='false'></i> Escríbeme</a><br>";    
       //Escribir en documento 
